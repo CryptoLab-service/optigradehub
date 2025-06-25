e8d997580a10b8498326a397f89911c64ca62554
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-06-25 22:23:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,13 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-  
+  document.querySelectorAll(".accordion-header").forEach(header => {
+  header.addEventListener("click", () => {
+    const item = header.parentElement;
+    item.classList.toggle("active");
+  });
+});
+
   // Optional: pause carousel on hover
   const carousel = document.querySelector(".carousel");
   carousel?.addEventListener("mouseenter", () => clearInterval(carouselInterval));
